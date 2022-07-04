0077d4c43596aa3c440bb521d73d6313fe3bfaae
Lee-jisang <dlwltkd2003@naver.com>
2022-07-04 17:15:48 +0900

    20220704

---
+++ Vue/Vue - The Complete Guide/basics-01-starting-code/basics-01-starting-code/app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      // always object return
+      courseGoalA: "Finish the course and learn Vue!",
+      courseGoalB: "<h1>Master the course and learn Vue!</h1>",
+      vueLink: "https://vuejs.org",
+    };
+  },
+  methods: {
+    ouputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
+    },
+  },
+});
+
+app.mount("#user-goal");
