69c313fd838a66707ec70ac702ee47e207e87e17
Lee-jisang <dlwltkd2003@naver.com>
2022-08-16 17:11:46 +0900

    0816

---
+++ JavaScript/momentom/20220816/app.js
@@ -0,0 +1,63 @@
+const title = document.querySelector(".title"); //querySelector
+const h2 = document.querySelector(".hello h2");
+
+function handleTitle() {
+  title.classList.toggle("active");
+}
+title.addEventListener("click", handleTitle);
+
+let clickMode = false;
+function clickEvent() {
+  clickMode = !clickMode;
+  clickMode ? (h2.className = "active") : (h2.className = "");
+}
+h2.addEventListener("click", clickEvent);
+
+let mode = false;
+function changeMode() {
+  mode = !mode;
+  if (mode) {
+    document.querySelector("body").style.backgroundColor = "black";
+    document.querySelector("body").style.color = "white";
+  } else {
+    document.querySelector("body").style.backgroundColor = "white";
+    document.querySelector("body").style.color = "black";
+  }
+  mode ? console.log(mode) : console.log(mode);
+}
+
+const loginForm = document.querySelector("#login-form"); // document.getElementById("login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginsubmit(event) {
+  event.preventDefault(); //이벤트의 기본행동, 브라우저의 기본동작을 막아줌
+  loginForm.classList.toggle(HIDDEN_CLASSNAME);
+  const userName = loginInput.value;
+  localStorage.setItem("username", userName);
+  paintGreetings(userName);
+}
+
+const alink = document.querySelector("a");
+function moveLink(event) {
+  event.preventDefault();
+  console.dir(event);
+}
+alink.addEventListener("click", moveLink);
+
+function paintGreetings(username) {
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerHTML = `Hello ${username}`;
+}
+
+const savedUsername = localStorage.getItem("username");
+
+if (savedUsername === null) {
+  loginForm.classList.toggle(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginsubmit); //addEventListener의 함수는 브라우저가 실행하는 함수임
+} else {
+  paintGreetings(savedUsername);
+}
+
+console.log(savedUsername);
